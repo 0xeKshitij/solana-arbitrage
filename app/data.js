04f05f80a6a9263e11f8892e09205ffb57169a09
Kshitij0O7 <ipanonymous885@gmail.com>
2024-07-05 14:14:17 +0530

    Initial Commit

---
+++ app/data.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const getData = async () => {
+  let data = JSON.stringify({
+    query:
+      'subscription {\n  Solana {\n    DEXTrades(\n      where: {Trade: {Buy: {Currency: {MintAddress: {is: "So11111111111111111111111111111111111111112"}}}, Sell: {Currency: {MintAddress: {is: "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"}}}}}\n    ) {\n      Trade {\n        Dex {\n          ProgramAddress\n          ProtocolName\n          ProtocolFamily\n        }\n        Buy {\n          PriceInUSD\n          Account {\n            Address\n          }\n        }\n      }\n      Block {\n        Time\n      }\n    }\n  }\n}\n',
+    variables: "{}",
+  });
+
+  let config = {
+    method: "post",
+    maxBodyLength: Infinity,
+    url: "https://streaming.bitquery.io/eap",
+    headers: {
+      "Content-Type": "application/json",
+      "X-API-KEY": "BQYuTITWanwYGz0YLGdcWSADO74o5RTX",
+      Authorization:
+        "Bearer ory_at_cZMaUvd9KNt998mR2NMAzqonPD8TvSY0snjgXgf08ls.c9DgfA5Quq5MJ3cMtPHGPYyMfetYkdag6GjtBee6UbY",
+    },
+    data: data,
+  };
+
+  let response = await axios.request(config);
+  //  console.log(JSON.stringify(response.data.data.Solana.DEXTrades))
+  return response.data.data.Solana.DEXTrades;
+};
+
+export default getData;
